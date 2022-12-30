5ee06110f97dc0f1bfcead9cfa51d201b3067022
chicaixiang <2932526804@qq.com>
2022-12-30 16:27:46 +0800

    调整地址

--- student_test/config.py
+++ student_test/config.py
@@ -3,7 +3,7 @@ import os
 
 import os
 
-URL = "http://127.0.0.1:8099/api/departments/"
+URL = "http://172.16.88.36:8099/api/departments/"
 PROJECT_DIR="D:\\chicaixiang\\auto-test\\Interface test\\student\\student_env"
 BASE_DIR = os.path.dirname(__file__)
 if __name__ =="__main__":
